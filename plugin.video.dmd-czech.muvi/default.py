f323940144eff2ee97d0ed0aaaff5c27d75a8f18
jirka.vyhnalek00@gmail.com <jirka.vyhnalek00@gmail.com@bb273897-6a10-2832-02c0-081a89f0f875>
2011-11-11 12:21:24 +0000

    - oprava muvi.cz
    git-svn-id: http://dmd-xbmc.googlecode.com/svn/trunk@70 bb273897-6a10-2832-02c0-081a89f0f875

--- plugin.video.dmd-czech.muvi/default.py
+++ plugin.video.dmd-czech.muvi/default.py
@@ -13,42 +13,39 @@ REV = os.path.join( profile, 'list_revision')
 icon = xbmc.translatePath( os.path.join( home, 'icon.png' ) )
 nexticon = xbmc.translatePath( os.path.join( home, 'nextpage.png' ) )
 fanart = xbmc.translatePath( os.path.join( home, 'fanart.jpg' ) )
-file = __settings__.getSetting('xml_file')
+
 
 
 def OBSAH():
-    addDir('Top20',__baseurl__+'/klipy/top20',1,icon)    
-    addDir('Nejnovější',__baseurl__+'/klipy/nejnovejsi',1,icon)    
-    addDir('Fox TV',__baseurl__+'/klipy/4',1,icon)
-    addDir('Seriály',__baseurl__+'/klipy/porad/37',1,icon)    
-    req = urllib2.Request(__baseurl__+'/porady/vsechny-porady')
+    zakazane = ['/aerokratas-2011']
+    addDir('Top20',__baseurl__+'/video/top20',1,icon)    
+    addDir('Nejnovější',__baseurl__+'/video/nejnovejsi',1,icon)    
+    addDir('Seriály',__baseurl__+'/video/porad/37',1,icon)    
+    req = urllib2.Request(__baseurl__+'/porady/vsechny')
     req.add_header('User-Agent', _UserAgent_)
     response = urllib2.urlopen(req)
     httpdata = response.read()
     response.close()
     match = re.compile('<a href="(.+?)"><img src="(.+?)" alt="(.+?)" /></a>').findall(httpdata)
     for url,thumb,name in match:
+        if url in zakazane:
+            continue
         addDir(name,__baseurl__+url,1,__baseurl__+thumb)    
 
             
-def INDEX(url):
+def INDEX(url): 
     doc = read_page(url)
-    items = doc.find('ul', 'item_list')
-    for item in items.findAll('li','clip'):
-        thumb = item.find('a', 'videoListImg')
-        thumb = thumb['style']
-        thumb = thumb[(thumb.find('url(') + len('url(') + 1):] 
-        thumb = thumb[:(thumb.find(')') - 1)]
-        info = item.find('div', 'text')
+    items = doc.find('div', 'videoClipsWrapper')
+    for item in items.findAll('li','listItem'):
+        thumb = item.find('a', 'framedThumbnail')
+        thumb = str(item.img['src'])            
+        info = item.find('div', 'carouselListItemText')
         name = info.find('a')
         name = name.getText(" ").encode('utf-8')
-        namex = info.find('a','interpreter')
-        try:
-            name2 = namex.find('font')
-            name2 = name2.getText(" ").encode('utf-8')
-        except:
-            name2 = namex.getText(" ").encode('utf-8')
-        link = str(info.a['href']) 
+        name2 = info.find('div','showTitle')
+        name2 = name2.getText(" ").encode('utf-8')
+        link = str(info.a['href'])
+        #print name+' '+name2,__baseurl__+link,2,thumb
         addDir(name+' '+name2,__baseurl__+link,2,thumb)    
     try:
         items = doc.find('div', 'pager')
@@ -58,33 +55,21 @@ def INDEX(url):
                 next_url = item['href'].replace('.','')
                 cast_url = urlparse(url)
                 #print 'http://'+cast_url[1]+cast_url[2]+next_url
-                addDir('= Další strana =','http://'+cast_url[1]+cast_url[2]+next_url,1,nexticon)
+                addDir('>> Další strana >>','http://'+cast_url[1]+cast_url[2]+next_url,1,nexticon)
     except:
-        print 'strankovani nenalezeno'        
+        print 'strankovani nenalezeno'              
 
 
 def VIDEOLINK(url):
     doc = read_page(url)
-    match = re.compile('low_id=(.+?)&high_id=(.+?)&').findall(str(doc))
     name = re.compile('<title>(.+?)</title>').findall(str(doc))
-    thumb = re.compile('<link rel="image_src" href="(.+?)"').findall(str(doc))    
-    for lowid,highid in match:
-        lowlink = 'http://83.167.246.23/video/getSource?id='+lowid
-        highlink = 'http://83.167.246.23/video/getSource?id='+highid
-        if __settings__.getSetting('kvalita_sel') == "false":
-            doc = read_page(lowlink)
-            config = re.compile('<server cache_id=".+?" proto="http" host="(.+?)" port="80" path="(.+?)"').findall(str(doc))
-            for server,cesta in config:
-                lq_link = 'http://'+server+'/'+cesta
-                #print 'LQ '+name[0],lq_link,thumb[0],name[0]
-                addLink('LQ '+name[0],lq_link,thumb[0],name[0])
-        if __settings__.getSetting('kvalita_sel') == "true":
-            doc = read_page(highlink)
-            config = re.compile('<server cache_id=".+?" proto="http" host="(.+?)" port="80" path="(.+?)"').findall(str(doc))
-            for server,cesta in config:
-                hq_link = 'http://'+server+'/'+cesta
-                print 'HQ '+name[0],hq_link,thumb[0],name[0]
-                addLink('HQ '+name[0],hq_link,thumb[0],name[0])
+    thumb = re.compile('<link rel="image_src" href="(.+?)"').findall(str(doc))
+    low_hq = re.compile("'file','(.+?)'").findall(str(doc))        
+    high_hq = re.compile("'hd.file','(.+?)'").findall(str(doc))    
+    if __settings__.getSetting('kvalita_sel') == "false":
+        addLink('LQ '+name[0],low_hq[0],thumb[0],name[0])
+    if __settings__.getSetting('kvalita_sel') == "true":
+        addLink('HQ '+name[0],high_hq[0],thumb[0],name[0])
 def get_params():
         param=[]
         paramstring=sys.argv[2]
