891f29c526d25a9fa2f7232a07086ef8704acf48
jirka.vyhnalek00@gmail.com <jirka.vyhnalek00@gmail.com@bb273897-6a10-2832-02c0-081a89f0f875>
2012-11-29 08:35:23 +0000

    - oprava resolveru vk.com
    git-svn-id: http://dmd-xbmc.googlecode.com/svn/trunk@140 bb273897-6a10-2832-02c0-081a89f0f875

--- script.module.dmd-czech.common/lib/vk.py
+++ script.module.dmd-czech.common/lib/vk.py
@@ -1,111 +1,59 @@
 # -*- coding: utf-8 -*-
-#------------------------------------------------------------
-# pelisalacarta - XBMC Plugin
-# Conector para VK Server
-# http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
+#-----------------------------------------------------------------------------
 #
-# Modify: 2012-05-10,  Ivo Brhel
+# Modify: 2012-11-28,  Ivo Brhel, 
 #
-#------------------------------------------------------------
+# based on vk resolver from http://code.google.com/p/xbmc-doplnky/
+#
+#------------------------------------------------------------------------------
 
 import urlparse,urllib2,urllib,re
 import os
 
 
-def geturl(url):
+def substr(data,start,end):
+	i1 = data.find(start)
+	i2 = data.find(end,i1)
+	return data[i1:i2]
+
+
+def getData(url):
     req = urllib2.Request(url)
     req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
     response = urllib2.urlopen(req)
-    link=response.read()
-    return link
+    data=response.read()
+    return data
 
 
-
-# Returns an array of possible video url's from the page_url
-def getURL( page_url , premium = False , user="" , password="", video_password="" ):
-    print("[vk.py] getURL(page_url='%s')" % page_url)
-
-    # Lee la pagina y extrae el ID del video
-    #data = scrapertools.cache_page(page_url.replace("amp;",""))
-    data = geturl(page_url.replace("amp;",""))
-    videourl = ""
-    regexp =re.compile(r'vkid=([^\&]+)\&')
-    match = regexp.search(data)
-    vkid = ""
-    if match is not None:
-        vkid = match.group(1)
-    else:
-        print("no vkid")
     
-    # Extrae los parametros del video y acade las calidades a la lista
-    patron  = "var video_host = '([^']+)'.*?"
-    patron += "var video_uid = '([^']+)'.*?"
-    patron += "var video_vtag = '([^']+)'.*?"
-    patron += "var video_no_flv = ([^;]+);.*?"
-    patron += "var video_max_hd = '([^']+)'"
-    matches = re.compile(patron,re.DOTALL).findall(data)
-    #scrapertools.printMatches(matches)
-
-    video_urls = []
-
-    if len(matches)>0:    
-        for match in matches:
-            if match[3].strip() == "0" and match[1] != "0":
-                tipo = "flv"
-                if "http://" in match[0]:
-                    videourl = "%s/u%s/video/%s.%s" % (match[0],match[1],match[2],tipo)
-                else:
-                    videourl = "http://%s/u%s/video/%s.%s" % (match[0],match[1],match[2],tipo)
-                
-                # Lo acade a la lista
-                video_urls.append( ["FLV [vk]",videourl])
-
-            elif match[1]== "0" and vkid != "":     #http://447.gt3.vkadre.ru/assets/videos/2638f17ddd39-75081019.vk.flv 
-                tipo = "flv"
-                if "http://" in match[0]:
-                    videourl = "%s/assets/videos/%s%s.vk.%s" % (match[0],match[2],vkid,tipo)
-                else:
-                    videourl = "http://%s/assets/videos/%s%s.vk.%s" % (match[0],match[2],vkid,tipo)
-                
-                # Lo acade a la lista
-                video_urls.append( ["FLV [vk]",videourl])
-                
-            else:                                   #http://cs12385.vkontakte.ru/u88260894/video/d09802a95b.360.mp4
-                #quality = config.get_setting("quality_flv")
-                quality = "1"
-                #Si la calidad elegida en el setting es HD se reproducira a 480 o 720, caso contrario solo 360, este control es por la xbox
-                if match[4]=="0":
-                    video_urls.append( ["240p [vk]",get_mp4_video_link(match[0],match[1],match[2],"240.mp4")])
-                elif match[4]=="1":
-                    video_urls.append( ["240p [vk]",get_mp4_video_link(match[0],match[1],match[2],"240.mp4")])
-                    video_urls.append( ["360p [vk]",get_mp4_video_link(match[0],match[1],match[2],"360.mp4")])
-                elif match[4]=="2":
-                    video_urls.append( ["240p [vk]",get_mp4_video_link(match[0],match[1],match[2],"240.mp4")])
-                    video_urls.append( ["360p [vk]",get_mp4_video_link(match[0],match[1],match[2],"360.mp4")])
-                    video_urls.append( ["480p [vk]",get_mp4_video_link(match[0],match[1],match[2],"480.mp4")])
-                elif match[4]=="3":
-                    video_urls.append( ["240p [vk]",get_mp4_video_link(match[0],match[1],match[2],"240.mp4")])
-                    video_urls.append( ["360p [vk]",get_mp4_video_link(match[0],match[1],match[2],"360.mp4")])
-                    video_urls.append( ["480p [vk]",get_mp4_video_link(match[0],match[1],match[2],"480.mp4")])
-                    video_urls.append( ["720p [vk]",get_mp4_video_link(match[0],match[1],match[2],"720.mp4")])
-                    video_urls.append( ["1080p [vk]",get_mp4_video_link(match[0],match[1],match[2],"1080.mp4")])
-                else:
-                    video_urls.append( ["240p [vk]",get_mp4_video_link(match[0],match[1],match[2],"240.mp4")])
-                    video_urls.append( ["360p [vk]",get_mp4_video_link(match[0],match[1],match[2],"360.mp4")])
-
-    for video_url in video_urls:
-        print("[vk.py] %s - %s" % (video_url[0],video_url[1]))
-
-    #return video_urls
-    result = video_urls[-1]
-    return result[1]
-    
-
-def get_mp4_video_link(match0,match1,match2,tipo):
-    if match0.endswith("/"):
-        videourl = "%su%s/video/%s.%s" % (match0,match1,match2,tipo)
-    else:
-        videourl = "%s/u%s/video/%s.%s" % (match0,match1,match2,tipo)
-    return videourl
-
-
+def getURL(page_url):
+	print("[vk.py] getURL(page_url='%s')" % page_url)
+        data = getData(page_url)
+        data = substr(data,'div id=\"playerWrap\"','<embed>')
+        if len(data) > 0:
+            host = re.search('host=([^\&]+)',data,re.IGNORECASE | re.DOTALL).group(1)
+            oid = re.search('oid=([^\&]+)',data,re.IGNORECASE | re.DOTALL).group(1)
+            uid = re.search('uid=([^\&]+)',data,re.IGNORECASE | re.DOTALL).group(1)
+            vtag = re.search('vtag=([^\&]+)',data,re.IGNORECASE | re.DOTALL).group(1)
+            hd = re.search('hd_def=([^\&]+)',data,re.IGNORECASE | re.DOTALL).group(1)
+            max_hd = re.search('hd=([^\&]+)',data,re.IGNORECASE | re.DOTALL).group(1)
+            no_flv = re.search('no_flv=([^\&]+)',data,re.IGNORECASE | re.DOTALL).group(1)
+            url = '%su%s/videos/%s' % (host,uid,vtag)
+            print("[vk.py] %s" % (url))
+            if no_flv != '1':
+                return [url+'.flv']
+            if no_flv == '1':
+                res=int(hd)
+                if max_hd:
+                    res=int(max_hd)
+                if res < 0:
+                    return [url+'.flv']
+                resolutions=['240','360','480','720','1080']
+                ret = []
+                for index,resolution in enumerate(resolutions):
+                    if index>res:
+                        return ret[-1]
+                    ret.append(url+'.'+resolution+'.mp4')
+                    print("[vk.py] %s - %s" % (resolution+'p',url+'.'+resolution+'.mp4'))
+                    
+        
