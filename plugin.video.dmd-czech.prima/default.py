c10bdd079ec331476056e4514824bd5e4593133f
jirka.vyhnalek00@gmail.com <jirka.vyhnalek00@gmail.com@bb273897-6a10-2832-02c0-081a89f0f875>
2013-04-11 18:04:01 +0000

    doplnek Prima - oprava stream serveru pro HD kvalitu
    git-svn-id: http://dmd-xbmc.googlecode.com/svn/trunk@157 bb273897-6a10-2832-02c0-081a89f0f875

--- plugin.video.dmd-czech.prima/default.py
+++ plugin.video.dmd-czech.prima/default.py
@@ -157,7 +157,7 @@ def VIDEOLINK(url,name):
         print keydata
         if geo_zone[0] == "1":
             #hd_url = 'rtmp://bcastgw.livebox.cz:80/iprima_token?auth=_any_'+keydata[1]+'/mp4:hq/'+hd_stream[0]
-            hd_url = 'rtmp://bcasts1w.livebox.cz:80/iprima_token_'+geo_zone[0]+'?auth=_any_'+keydata[1]+' playpath=mp4:hq/'+hd_stream+ ' live=true'
+            hd_url = 'rtmp://bcastgw.livebox.cz:80/iprima_token_'+geo_zone[0]+'?auth=_any_'+keydata[1]+' playpath=mp4:hq/'+hd_stream+ ' live=true'
             hq_url = 'rtmp://bcastgw.livebox.cz:80/iprima_token_'+geo_zone[0]+'?auth=_any_'+keydata[1]+'/mp4:'+hq_stream[0]
             lq_url = 'rtmp://bcastgw.livebox.cz:80/iprima_token_'+geo_zone[0]+'?auth=_any_'+keydata[1]+'/mp4:'+lq_stream[0]
             if __settings__.getSetting('proxy_use')  == "true":
@@ -169,7 +169,7 @@ def VIDEOLINK(url,name):
         else:
             if re.match('Prima', hq_stream[0], re.U): 
                 #hd_url = 'rtmp://bcastgw.livebox.cz:80/iprima_token?auth=_any_'+keydata[1]+'/mp4:hq/'+hd_stream[0]
-                hd_url = 'rtmp://bcasts1w.livebox.cz:80/iprima_token?auth=_any_'+keydata[1]+' playpath=mp4:hq/'+hd_stream+ ' live=true'
+                hd_url = 'rtmp://bcastgw.livebox.cz:80/iprima_token?auth=_any_'+keydata[1]+' playpath=mp4:hq/'+hd_stream+ ' live=true'
                 hq_url = 'rtmp://bcastgw.livebox.cz:80/iprima_token?auth=_any_'+keydata[1]+'/mp4:'+hq_stream[0]
                 lq_url = 'rtmp://bcastgw.livebox.cz:80/iprima_token?auth=_any_'+keydata[1]+'/mp4:'+lq_stream[0]
             else:
